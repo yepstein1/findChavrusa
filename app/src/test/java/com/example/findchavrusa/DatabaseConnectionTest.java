c024bcf7bd0408c11f04c5466a91bb47f97c1499
Yaakov Epstein <31752060+yepstien1@users.noreply.github.com>
2020-01-18 19:07:01 -0500

    having trouble with database connections- some changes and more files included.

--- app/src/test/java/com/example/findchavrusa/DatabaseConnectionTest.java
+++ app/src/test/java/com/example/findchavrusa/DatabaseConnectionTest.java
@@ -2,14 +2,12 @@ package com.example.findchavrusa;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class DatabaseConnectionTest {
     @Test
     public void addition_isCorrect() {
         DatabaseConnection db = new DatabaseConnection();
